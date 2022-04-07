fa881d9be5446ed03eaef22bcbb38c9e4e970023
Caleb Albers <calbers@hashicorp.com>
2022-04-07 16:18:07 -0700

    Add vpc_id output
    Snuck in a small output for vpc_id

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "security_groups" {
   description = "Security groups associated with ECS cluster."
   value       = local.security_groups
 }
+
+output "vpc_id" {
+  description = "VPC ID associated with ECS cluster. If create_vpc is false, output is an empty string."
+  value       = var.create_vpc ? module.vpc[0].vpc_id : ""
+}
